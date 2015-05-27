820fa1a55799aea32e2cfe6fad6b2a6a976914f1
Nick Luo <luotianqi@dihmed.com>
2015-05-28 07:44:34 +0800

    Update CommStructures

--- iValuableController/CommStructures.cpp
+++ iValuableController/CommStructures.cpp
@@ -1,20 +1,48 @@
 #include "CommStructures.h"
 
-namespace IntelliStorage
+namespace SerializableObjects
 {
 	CLAIM_CLASS(HeartBeat);
-	CLAIM_CLASS(NodeQuery);
-	CLAIM_CLASS(NodeList);
+	CLAIM_CLASS(DeviceDesc);
+	CLAIM_CLASS(CalWeight);
+	CLAIM_CLASS(SensorEnable);
+	CLAIM_CLASS(RawData);
+	CLAIM_CLASS(RawDataCollection);
+	CLAIM_CLASS(RampValue);
+	CLAIM_CLASS(RampCollection);
+	CLAIM_CLASS(ScaleState);
+	CLAIM_CLASS(UnitEntry);
+	CLAIM_CLASS(UnitEntryCollection);
 	CLAIM_CLASS(RfidDataBson);
 	CLAIM_CLASS(CommandResult);
+	
+	CLAIM_CLASS(ScaleAttribute);
+	CLAIM_CLASS(SuppliesItem);
+	CLAIM_CLASS(SuppliesCollection);
+	CLAIM_CLASS(InventoryQuantity);
+	CLAIM_CLASS(InventoryCollection);
 
 	void CommStructures::Register()
 	{
 		REGISTER_CLASS(HeartBeat);
-		REGISTER_CLASS(NodeQuery);
-		REGISTER_CLASS(NodeList);
+		REGISTER_CLASS(DeviceDesc);
+		REGISTER_CLASS(CalWeight);
+		REGISTER_CLASS(SensorEnable);
+		REGISTER_CLASS(RawData);
+		REGISTER_CLASS(RawDataCollection);
+		REGISTER_CLASS(RampValue);
+		REGISTER_CLASS(RampCollection);
+		REGISTER_CLASS(ScaleState);
+		REGISTER_CLASS(UnitEntry);
+		REGISTER_CLASS(UnitEntryCollection);
 		REGISTER_CLASS(RfidDataBson);
 		REGISTER_CLASS(CommandResult);
+		
+		REGISTER_CLASS(ScaleAttribute);
+		REGISTER_CLASS(SuppliesItem);
+		REGISTER_CLASS(SuppliesCollection);
+		REGISTER_CLASS(InventoryQuantity);
+		REGISTER_CLASS(InventoryCollection);
 	}
 }
 	
--- iValuableController/CommStructures.h
+++ iValuableController/CommStructures.h
@@ -4,16 +4,33 @@
 #include "TypeInfoBase.h"
 #include <cstdint>
 
-namespace IntelliStorage
+namespace SerializableObjects
 {
 	enum CodeType
 	{
-		HeartBeatCode = 0x00,          //class HeartBeat
-		QueryNodeId = 0x04,     //class NodeList
-		RfidDataCode = 0x0E,        //class RfidData
-		CommandResponse = 0x14,
-		QueryRfid = 0x15,
-		WhoAmICode = 0xff,
+		CodeZero						 	= 0x00,
+		CodeSetCalWeight			= 0x01,
+		CodeCalibration 			= 0x02,
+		CodeQueryAD 					= 0x03,
+		CodeQueryRamp 				= 0x04,
+		CodeSetSensorEnable 	= 0x05,
+		CodeSetSensorConfig 	= 0x06,
+		CodeTare 							= 0x07,
+		CodeHeartBeat 				= 0x08,
+		CodeReportRfid				= 0x09,
+		CodeReportDoor				= 0x0A,
+		CodeQueryState				= 0x0B,
+		CodeSetInventoryInfo		= 0x0C,
+		CodeQueryInventoryInfo	= 0x0D,
+		CodeSetInventory				= 0x0E,
+		CodeLockControl					= 0x0F,
+		CodeGuide								= 0x10,
+		
+		CodeQueryConfig					= 0xF0,
+		CodeQuerySensorEnable   = 0xF1,
+		CodeQueryInventory			= 0xF2,
+		CodeCommandResult				= 0xFE,
+		CodeWhoAmI							= 0xFF,	
 	};
 
 	DECLARE_CLASS(HeartBeat)
@@ -21,32 +38,188 @@ namespace IntelliStorage
 		public:
 			HeartBeat()
 			{
-				REGISTER_FIELD(times);
+				REGISTER_FIELD(Times);
 			}
 			virtual ~HeartBeat() {}
-			std::uint64_t times;
+			std::uint64_t Times;
 	};
-
-	DECLARE_CLASS(NodeQuery)
+	
+	DECLARE_CLASS(DeviceDesc)
 	{
 		public:
-			NodeQuery()
+			DeviceDesc()
 			{
-				REGISTER_FIELD(NodeId);
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
 			}
-			virtual ~NodeQuery() {}
-			int NodeId;
+			virtual ~DeviceDesc() {}
+			int GroupIndex;
+			int NodeIndex;
 	};
-
-	DECLARE_CLASS(NodeList)
+	
+	DECLARE_CLASS(CalWeight)
 	{
 		public:
-			NodeList()
+			CalWeight()
 			{
-				REGISTER_FIELD(NodeIds);
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(Weight);
 			}
-			virtual ~NodeList() {}
-			Array<int> NodeIds;
+			virtual ~CalWeight() {}
+			int GroupIndex;
+			int NodeIndex;
+			float Weight;
+	};
+	
+	DECLARE_CLASS(SensorEnable)
+	{
+		public:
+			SensorEnable()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(Flags);
+			}
+			virtual ~SensorEnable() {}
+			int GroupIndex;
+			int NodeIndex;
+			int Flags;
+	};
+	
+	DECLARE_CLASS(RawData)
+	{
+		public:
+			RawData()
+			{
+				REGISTER_FIELD(SensorIndex);
+				REGISTER_FIELD(ZeroAdCount);
+				REGISTER_FIELD(TareAdCount);
+				REGISTER_FIELD(CurrentAdCount);
+			}
+			virtual ~RawData() {}
+			int SensorIndex;
+			int ZeroAdCount;
+			int TareAdCount;
+			int CurrentAdCount;
+	};
+	
+	DECLARE_CLASS(RawDataCollection)
+	{
+		public:
+			RawDataCollection()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(AdCountValues);
+			}
+			virtual ~RawDataCollection() {}
+			int GroupIndex;
+			int NodeIndex;
+			Array<RawData> AdCountValues;
+	};
+	
+	DECLARE_CLASS(RampValue)
+	{
+		public:
+			RampValue()
+			{
+				REGISTER_FIELD(SensorIndex);
+				REGISTER_FIELD(Value);
+			}
+			virtual ~RampValue() {}
+			int SensorIndex;
+			float Value;
+	};
+	
+	DECLARE_CLASS(RampCollection)
+	{
+		public:
+			RampCollection()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(OneGramIncrements);
+			}
+			virtual ~RampCollection() {}
+			int GroupIndex;
+			int NodeIndex;
+			Array<RampValue> OneGramIncrements;
+	};
+	
+	DECLARE_CLASS(InventoryQuantity)
+	{
+		public:
+			InventoryQuantity()
+			{
+				REGISTER_FIELD(MaterialId);
+				REGISTER_FIELD(Quantity);
+			}
+			virtual ~InventoryQuantity() {}
+			std::uint64_t MaterialId;
+			int Quantity;
+	};
+	
+	DECLARE_CLASS(InventoryCollection)
+	{
+		public:
+			InventoryCollection()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(AllMaterialAmounts);
+			}
+			virtual ~InventoryCollection() {}
+			int GroupIndex;
+			int NodeIndex;
+			Array<InventoryQuantity> AllMaterialAmounts;
+	};
+	
+	DECLARE_CLASS(ScaleState)
+	{
+		public:
+			ScaleState()
+			{
+				REGISTER_FIELD(Weight);
+				REGISTER_FIELD(WeightState);
+				REGISTER_FIELD(IsDamaged);
+				REGISTER_FIELD(IsStable);
+			}
+			virtual ~ScaleState() {}
+			float Weight;
+			int WeightState;
+			bool IsDamaged;
+			bool IsStable;
+	};
+	
+	DECLARE_CLASS(UnitEntry)
+	{
+		public:
+			UnitEntry()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(Weight);
+				REGISTER_FIELD(IsStable);
+				REGISTER_FIELD(SensorStates);
+			}
+			virtual ~UnitEntry() {}
+			int GroupIndex;
+			int NodeIndex;
+			float Weight;
+			bool IsStable;
+			Array<ScaleState> SensorStates;
+	};
+	
+	DECLARE_CLASS(UnitEntryCollection)
+	{
+		public:
+			UnitEntryCollection()
+			{
+				REGISTER_FIELD(AllUnits);
+			}
+			virtual ~UnitEntryCollection() {}
+			Array<UnitEntry> AllUnits;
 	};
 
 	DECLARE_CLASS(RfidDataBson)
@@ -54,16 +227,16 @@ namespace IntelliStorage
 		public:
 			RfidDataBson()
 			{
-				REGISTER_FIELD(NodeId);
-				REGISTER_FIELD(State);
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(CardType);
 				REGISTER_FIELD(CardId);
-				REGISTER_FIELD(PresId);
 			}
 			virtual ~RfidDataBson() {}
-			int NodeId;
-			int State;
+			int GroupIndex;
+			int NodeIndex;
+			int CardType;
 			std::string CardId;
-			std::string PresId;
 	};
 	
 	DECLARE_CLASS(CommandResult)
@@ -71,14 +244,74 @@ namespace IntelliStorage
 		public:
 			CommandResult()
 			{
-				REGISTER_FIELD(NodeId);
-				REGISTER_FIELD(Command);
-				REGISTER_FIELD(Result);
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(RequestDemandType);
+				REGISTER_FIELD(IsOk);
 			}
 			virtual ~CommandResult() {}
-			int NodeId;
-			int Command;
-			bool Result;
+			int GroupIndex;
+			int NodeIndex;
+			int RequestDemandType;
+			bool IsOk;
+	};
+	
+	DECLARE_CLASS(ScaleAttribute)
+	{
+		public:
+			ScaleAttribute()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(Sensitivity);
+				REGISTER_FIELD(TempDrift);
+				REGISTER_FIELD(ZeroRange);
+				REGISTER_FIELD(SafeOverload);
+				REGISTER_FIELD(MaxOverload);
+			}
+			virtual ~ScaleAttribute() {}
+			int GroupIndex;
+			int NodeIndex;
+			float Sensitivity;
+			float TempDrift;
+			float ZeroRange;
+			float SafeOverload;
+			float MaxOverload;
+	};
+	
+	DECLARE_CLASS(SuppliesItem)
+	{
+		public:
+			SuppliesItem()
+			{
+				REGISTER_FIELD(SensorIndex);
+				REGISTER_FIELD(MaterialId);
+				REGISTER_FIELD(UnitWeight);
+				REGISTER_FIELD(ErrorRange);
+				REGISTER_FIELD(MaterialName);
+			}
+			virtual ~SuppliesItem() {}
+
+			int SensorIndex;
+			std::uint64_t MaterialId;
+			float UnitWeight;
+			float ErrorRange;
+			Binary MaterialName;
+	};
+	
+	DECLARE_CLASS(SuppliesCollection)
+	{
+		public:
+			SuppliesCollection()
+			{
+				REGISTER_FIELD(GroupIndex);
+				REGISTER_FIELD(NodeIndex);
+				REGISTER_FIELD(AllMaterialInfos);
+			}
+			virtual ~SuppliesCollection() {}
+			int GroupIndex;
+			int NodeIndex;
+			Array<SuppliesItem> AllMaterialInfos;
 	};
 	
 	class CommStructures
