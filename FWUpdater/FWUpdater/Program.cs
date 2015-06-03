5ea5ad3be76540a6d1bd9de8b184947bbe445633
Nick Luo <luotianqi@dihmed.com>
2015-06-03 17:36:52 +0800

    Add FWUpdater Command Line Tool

---
+++ FWUpdater/FWUpdater/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWUpdater
+{
+    class Program
+    {
+        public readonly SerialComm Comm;
+
+        public Program()
+        {
+            var port = ConfigurationManager.AppSettings["PORT"];
+            var ports = SerialPort.GetPortNames();
+            if (ports.Length == 0)
+            {
+                Console.WriteLine("No available comm port!");
+                Console.ReadLine();
+                return;
+            }
+            if (port == "AUTO")
+                port = ports[0];
+            if (ports.All(p => p != port))
+            {
+                Console.WriteLine("No available comm port!");
+                Console.ReadLine();
+                return;
+            }
+            Comm = new SerialComm(port);
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters");
+                Console.WriteLine("Available commands: ");
+                Console.WriteLine(" Write (index) (type) (version) (filepath)");
+                Console.WriteLine(" Info [(index)]");
+                Console.WriteLine(" Update All");
+                Console.WriteLine(" Update (type)");
+                Console.WriteLine(" Update (id) [(type)]");
+                Console.WriteLine("Parameters comment: ");
+                Console.WriteLine(" (index): 0 to 3 for memory slot");
+                Console.WriteLine(" (type): Unity/Indpt/Rfid");
+                Console.WriteLine(" (version): nn.nn");
+                Console.WriteLine(" (filepath): binary executable file");
+                Console.WriteLine(" (id): 0 to 127 for node id specified");
+                return;
+            }
+            byte[] data;
+            var program = new Program();
+            CommandType command;
+            if (!Enum.TryParse(args[0], out command))
+            {
+                Console.WriteLine("Invalid Command!");
+                return;
+            }
+            switch (command)
+            {
+                case CommandType.Write:
+                    break;
+                case CommandType.Info:
+                    break;
+                case CommandType.Update:
+                    break;
+                default:
+                    Console.WriteLine("Invalid Command!");
+                    return;
+            }
+        }
+    }
+}
---
+++ FWUpdater/FWUpdater/SerialComm.cs
@@ -0,0 +1,150 @@
+﻿using System.IO.Ports;
+using System.Threading;
+
+namespace FWUpdater
+{
+    public enum CommandType : byte
+    {
+        Access = 0xc2,
+        Write = 0xc4,
+        WriteInfo = 0xce,
+        Info = 0xcf,
+        Update = 0xd0,
+        Status = 0xfe
+    };
+
+    class SerialComm
+    {
+        private enum StateType : byte
+        {
+            Delimiter1,
+            Delimiter2,
+            FileCommand,
+            FileDataLength,
+            FileData,
+            Checksum,
+        };
+
+        private readonly SerialPort serial;
+        //public event DataArrival DataArrivalEvent;
+        private CommandType resultCommand;
+        private byte[] resultData;
+
+        public SerialComm(string port)
+        {
+            serial = new SerialPort(port, 115200, Parity.None, 8, StopBits.One)
+            {
+                Handshake = Handshake.None, //Handshake not needed   
+            };
+            serial.DataReceived += SerialDataReceived;
+            serial.Open();
+            serial.DiscardOutBuffer();
+            serial.DiscardInBuffer();
+        }
+
+        private StateType dataState = StateType.Delimiter1;
+        private byte sumcheck;
+        private byte command;
+        private ushort readIndex;
+        private ushort readLength;
+        private static readonly byte[] Header = { 0xfe, 0xfa, 0xfb };
+        private readonly AutoResetEvent syncEvent = new AutoResetEvent(false);
+
+        private byte[] recieveData;
+
+        private void SerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var count = serial.BytesToRead;
+            while (count > 0)
+            {
+                var b = (byte)serial.ReadByte();
+                switch (dataState)
+                {
+                    case StateType.Delimiter1:
+                        if (b == Header[0])
+                            dataState = StateType.Delimiter2;
+                        break;
+                    case StateType.Delimiter2:
+                        if (b == Header[2])
+                        {
+                            dataState = StateType.FileCommand;
+                            sumcheck = (byte)(Header[0] + Header[2]);
+                        }
+                        else
+                            dataState = StateType.Delimiter1;
+                        break;
+                    case StateType.FileCommand:
+                        sumcheck += b;
+                        command = b;
+                        readIndex = 0;
+                        readLength = 0;
+                        dataState = StateType.FileDataLength;
+                        break;
+                    case StateType.FileDataLength:
+                        sumcheck += b;
+                        readLength |= (ushort)(b << (readIndex << 3));
+                        if (++readIndex > 1)
+                        {
+                            if (readLength == 0)
+                            {
+                                dataState = StateType.Checksum;
+                            }
+                            else
+                            {
+                                dataState = StateType.FileData;
+                                recieveData = new byte[readLength];
+                                readIndex = 0;
+                            }
+                        }
+                        break;
+                    case StateType.FileData:
+                        sumcheck += b;
+                        recieveData[readIndex++] = b;
+                        if (readIndex >= readLength)
+                            dataState = StateType.Checksum;
+                        break;
+                    case StateType.Checksum:
+                        if (sumcheck == b)
+                        {
+                            resultCommand = (CommandType)command;
+                            resultData = recieveData;
+                            syncEvent.Set();
+                            //if (DataArrivalEvent != null)
+                            //    DataArrivalEvent((CommandType) command, recieveData);
+                        }
+                        dataState = StateType.Delimiter1;
+                        break;
+                }
+                count--;
+            }
+        }
+
+        public byte[] SendCommand(CommandType code, byte[] data = null)
+        {
+            Send(code, data);
+            syncEvent.WaitOne(Timeout.Infinite);
+            return resultCommand == code ? resultData : null;
+        }
+
+        private void Send(CommandType com, byte[] data)
+        {
+            var len = 0;
+            if (data != null)
+                len = data.Length;
+            var pre = new[] { Header[0], Header[1], (byte)com, (byte)(len & 0xff), (byte)((len >> 8) & 0xff) };
+            byte checksum = 0;
+            for (var i = 0; i < 5; i++)
+                checksum += pre[i];
+            for (var i = 0; i < len; i++)
+                if (data != null)
+                    checksum += data[i];
+            serial.Write(pre, 0, 5);
+            if (len > 0 && data != null)
+                serial.Write(data, 0, len);
+            pre[0] = checksum;
+            serial.Write(pre, 0, 1);
+        }
+    }
+}
+
+
