20cec6cf53fd3e26389d135073df896ee5a9730d
Nick Luo <luotianqi@dihmed.com>
2015-06-07 21:21:23 +0800

    Add CRC32 algorithm for FWUpdater

---
+++ FWUpdater/FWUpdater/Crc32.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace FWUpdater
+{
+    public class Crc32
+    {
+        private const uint KCrcPoly = 0xEDB88320u; //0x04C11DB7u;//
+        private const uint KInitial = 0xFFFFFFFF;
+        private static readonly uint[] Table;
+        private const uint CrcNumTables = 8;
+
+        static Crc32()
+        {
+            unchecked
+            {
+                Table = new uint[256 * CrcNumTables];
+                uint i;
+                for (i = 0; i < 256; i++)
+                {
+                    var r = i;
+                    for (var j = 0; j < 8; j++)
+                        r = (r >> 1) ^ (KCrcPoly & ~((r & 1) - 1));
+                    Table[i] = r;
+                }
+                for (; i < 256 * CrcNumTables; i++)
+                {
+                    var r = Table[i - 256];
+                    Table[i] = Table[r & 0xFF] ^ (r >> 8);
+                }
+            }
+        }
+
+        private uint value;
+
+        public Crc32()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Reset CRC
+        /// </summary>
+        public void Init()
+        {
+            value = KInitial;
+        }
+
+        public int Value
+        {
+            //get { return (int)~value; }
+            get { return (int)(value^KInitial); }
+        }
+
+        public void UpdateByte(byte b)
+        {
+            value = (value >> 8) ^ Table[(byte)value ^ b];
+        }
+
+        public void Update(byte[] data, int offset, int count)
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            new ArraySegment<byte>(data, offset, count);     // check arguments
+            if (count == 0) return;
+
+            var table = Table;        // important for performance!
+
+            var crc = value;
+
+            for (; (offset & 7) != 0 && count != 0; count--)
+                crc = (crc >> 8) ^ table[(byte)crc ^ data[offset++]];
+
+            if (count >= 8)
+            {
+                /*
+                 * Idea from 7-zip project sources (http://7-zip.org/sdk.html)
+                 */
+
+                var to = (count - 8) & ~7;
+                count -= to;
+                to += offset;
+
+                while (offset != to)
+                {
+                    crc ^= (uint)(data[offset] + (data[offset + 1] << 8) + (data[offset + 2] << 16) + (data[offset + 3] << 24));
+                    var high = (uint)(data[offset + 4] + (data[offset + 5] << 8) + (data[offset + 6] << 16) + (data[offset + 7] << 24));
+                    offset += 8;
+
+                    crc = table[(byte)crc + 0x700]
+                        ^ table[(byte)(crc >>= 8) + 0x600]
+                        ^ table[(byte)(crc >>= 8) + 0x500]
+                        ^ table[/*(byte)*/(crc >> 8) + 0x400]
+                        ^ table[(byte)(high) + 0x300]
+                        ^ table[(byte)(high >>= 8) + 0x200]
+                        ^ table[(byte)(high >>= 8) + 0x100]
+                        ^ table[/*(byte)*/(high >> 8) + 0x000];
+                }
+            }
+
+            while (count-- != 0)
+                crc = (crc >> 8) ^ table[(byte)crc ^ data[offset++]];
+
+            value = crc;
+        }
+
+        static public int Compute(byte[] data, int offset, int size)
+        {
+            var crc = new Crc32();
+            crc.Update(data, offset, size);
+            return crc.Value;
+        }
+
+        static public int Compute(byte[] data)
+        {
+            return Compute(data, 0, data.Length);
+        }
+
+        static public int Compute(ArraySegment<byte> block)
+        {
+            return Compute(block.Array, block.Offset, block.Count);
+        }
+    }
+}
