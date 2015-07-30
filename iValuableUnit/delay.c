e40cb5458418d304dac6fdd74f6224cc82ec3845
Nick Luo <luotianqi@dihmed.com>
2015-07-30 20:10:30 +0800

    Fix delay() interval

--- iValuableUnit/delay.c
+++ iValuableUnit/delay.c
@@ -5,6 +5,7 @@ __asm void SysCtlDelay(unsigned long ulCount)
 {
     subs    r0, #1;
 		nop;
+	  nop;
     bne     SysCtlDelay;
     bx      lr;
 }
--- iValuableUnit/delay.h
+++ iValuableUnit/delay.h
@@ -5,7 +5,7 @@ extern "C" {
 #endif
 	
 extern void SysCtlDelay(unsigned long ulCount);
-#define DELAY(us) SysCtlDelay((SystemCoreClock/8000000)*(us))
+#define DELAY(us) SysCtlDelay((SystemCoreClock/6000000)*(us))
 
 	
 #ifdef __cplusplus
